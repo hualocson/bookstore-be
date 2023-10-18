313487cd31515fcc8d2920482615140850ac9c56
locson <hualocson@gmail.com>
2023-10-18 21:55:29 +0700

    UPDATE: add cart api

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./routes/auth.routes";
+import cartRoutes from "./routes/cart.routes";
 
 export default () => {
   const router = Router();
 
   authRoutes(router);
+  cartRoutes(router);
 
   return router;
 };
