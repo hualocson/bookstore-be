7b879da3d2d1c524757eda1a7c2b941a712ebdcc
locson <hualocson@gmail.com>
2023-10-18 21:54:56 +0700

    UPDATE: server load redis before listen request

--- src/index.js
+++ src/index.js
@@ -3,34 +3,41 @@ import startApp from "@/app";
 import logger from "@/configs/logger";
 import configs from "@/configs/vars";
 
-const server = http.createServer(startApp());
+startApp()
+  .then((app) => {
+    const server = http.createServer(app);
 
-server.listen(configs.port, () => {
-  logger.info(`Listening to port ${configs.port}`);
-});
-
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info("Server closed");
-      process.exit(1);
+    server.listen(configs.port, () => {
+      logger.info(`Listening to port ${configs.port}`);
     });
-  } else {
+
+    const exitHandler = () => {
+      if (server) {
+        server.close(() => {
+          logger.info("Server closed");
+          process.exit(1);
+        });
+      } else {
+        process.exit(1);
+      }
+    };
+
+    const unexpectedErrorHandler = (error) => {
+      logger.error(error);
+      exitHandler();
+    };
+
+    process.on("uncaughtException", unexpectedErrorHandler);
+    process.on("unhandledRejection", unexpectedErrorHandler);
+
+    process.on("SIGTERM", () => {
+      logger.info("SIGTERM received");
+      if (server) {
+        server.close();
+      }
+    });
+  })
+  .catch((err) => {
+    logger.error(err);
     process.exit(1);
-  }
-};
-
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on("uncaughtException", unexpectedErrorHandler);
-process.on("unhandledRejection", unexpectedErrorHandler);
-
-process.on("SIGTERM", () => {
-  logger.info("SIGTERM received");
-  if (server) {
-    server.close();
-  }
-});
+  });
