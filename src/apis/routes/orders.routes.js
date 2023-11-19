c11d5c613a5611a59d3988d0c97947e2a17f8e12
locson <hualocson@gmail.com>
2023-11-19 11:27:11 +0700

    UPDATE: route for order history

--- src/apis/routes/orders.routes.js
+++ src/apis/routes/orders.routes.js
@@ -6,6 +6,8 @@ import isAuth from "../middlewares/isAuth";
  * @param {import("express").Router} router
  */
 const ordersRoutes = (router) => {
+  router.get("/orders", isAuth, ordersController.getOrders);
+
   router.post("/orders", isAuth, ordersController.createOrder);
 };
 
