e3d69ba32c6e0e88bd7dc43af20b4d37c8afd204
locson <hualocson@gmail.com>
2023-12-04 21:28:16 +0700

    ADD: logic handle favorites

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import categoriesRoutes from "@/apis/routes/categories.routes";
 import customerRoutes from "@/apis/routes/customers.routes";
+import favoritesRoutes from "@/apis/routes/favorites.routes";
 import productsRoutes from "@/apis/routes/products.routes";
 import { Router } from "express";
 import addressesRoutes from "./routes/addresses.routes";
@@ -21,5 +22,6 @@ export default () => {
   productsRoutes(router);
   categoriesRoutes(router);
   customerRoutes(router);
+  favoritesRoutes(router);
   return router;
 };
